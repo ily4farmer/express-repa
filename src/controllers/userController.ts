a00de77b13dbb2e87d5ef9bb658209c348e66be3
Lexa <ily4.farmer@yandex.ru>
2022-11-10 17:53:38 +0300

    add prettier config

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -3,27 +3,29 @@ import { User } from "../models/models";
 
 class userContoller {
     async getUser(req: Request, res: Response, next: NextFunction) {
-        const users = await User.findAll()
-        return res.json(users)
+        const users = await User.findAll();
+        return res.json(users);
     }
+
     async addUser(req: Request, res: Response) {
-        const {name} = req.body
+        const { name } = req.body;
         const list = await User.create({
             name
-        })
-        res.json(list)
+        });
+        res.json(list);
     }
 
     async deleteUser(req: Request, res: Response) {
-        const { id } = req.body
+        const { id } = req.body;
 
-        const result = await User.destroy({where: {
-            id: id
-        }})
+        const result = await User.destroy({
+            where: {
+                id: id
+            }
+        });
 
-        res.json(result)
-
+        res.json(result);
     }
 }
 
-export default new userContoller()
+export default new userContoller();
